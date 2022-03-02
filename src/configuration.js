b92fc875bebe2398bda7c2f7c61c042bfcf25473
Tanya933 <tanya.rajpal0400@gmail.com>
2022-03-02 13:19:35 +0530

    minor changes

--- src/configuration.js
+++ src/configuration.js
@@ -1,4 +1,4 @@
 module.exports = {
-    // base_backend_url : "http://localhost:4000/api/examination-helper"
-    base_backend_url : "https://examination-helper-backend-production.up.railway.app/api/examination-helper"
+     base_backend_url : "http://localhost:4000/api/examination-helper"
+    //base_backend_url : "https://examination-helper-backend-production.up.railway.app/api/examination-helper"
 }
